b27d213e1ab3559337899b2b7e31a198d0ec7e58
EsterSPR <web.esterspreafico@gmail.com>
2023-02-17 16:29:47 +0100

    aggiunti contenuti minimi header e footer no stile

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,15 @@
-<h1>footer</h1>
+<footer>
+
+    <div class="row w-100 bg-primary m-0">
+        Lorem
+    </div>
+
+    <div class="row w-100 bg-secondary m-0">
+        Lorem
+    </div>
+
+    <div class="row w-100 bg-primary m-0">
+        Lorem
+    </div>
+    
+</footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,74 @@
-<h1>header</h1>
+<header>
+    <div class="row w-100 m-0 bg-primary">
+        DC POWER VISA
+    </div>
+
+    <div class="row w-100 m-0">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="" alt="dc-logo"></a>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                <li class="nav-item">
+                <a class="nav-link" href="#">CHARACTERS</a>
+                </li>
+
+                <li class="nav-item">
+                <a class="nav-link active" href="#">COMICS</a>
+                </li>
+
+                <li class="nav-item">
+                <a class="nav-link" href="#">MOVIES</a>
+                </li>
+
+                <li class="nav-item">
+                <a class="nav-link" href="#">TV</a>
+                </li>
+
+                <li class="nav-item">
+                <a class="nav-link" href="#">GAMES</a>
+                </li>
+
+                <li class="nav-item">
+                <a class="nav-link" href="#">COLLECTIBLES</a>
+                </li>
+
+                <li class="nav-item">
+                <a class="nav-link" href="#">VIDEOS</a>
+                </li>
+
+                <li class="nav-item">
+                <a class="nav-link" href="#">FANS</a>
+                </li>
+
+                <li class="nav-item">
+                <a class="nav-link" href="#">NEWS</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        SHOP
+                    </a>
+
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">SHOP 1</a></li>
+                        <li><a class="dropdown-item" href="#">SHOP 2</a></li>
+                        <li><a class="dropdown-item" href="#">SHOP 3</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+
+            </div>
+        </div>
+        </nav>
+    </div>
+  </header>
