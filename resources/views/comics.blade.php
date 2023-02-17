b984fbd7e7268f2a15a90fd1ee7b91816e8e0091
EsterSPR <web.esterspreafico@gmail.com>
2023-02-17 18:09:35 +0100

    passati fumetti in view

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -16,7 +16,28 @@
 
     @include('partials.header')
 
-    MAIN
+    <div class="container">
+
+        <div class="row w-100 m-0 row-cols-1 g-4"></div>
+
+        <div class="row w-100 m-0 row-cols-1 g-4">
+
+            @foreach($comicard as $key => $comicardsingle)
+
+            <div class="col-3"><a href="#">
+                <div class="card h-100 border-0">
+                    <img src="{{ $comicardsingle['thumb'] }}" class="card-img-top" alt="{{ $comicardsingle['title'] }}">
+                    <div class="card-body px-0">
+                        <h5 class="card-title es_cardlink">{{ $comicardsingle['title'] }}</h5>
+                    </div>
+                </div>
+            </a></div>
+
+            @endforeach
+
+        </div>
+
+    </div>
 
     @include('partials.footer')
 
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('comics');
-});
+    $comicard = config('comics');
+    return view('comics', compact('comicard'));
+}) ->name('comicard');
